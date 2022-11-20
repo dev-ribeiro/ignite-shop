92c0ea6d7288abefb3b6282e74c99bc1fd11b8ad
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-20 08:44:37 -0300

    lesson: config stitches lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+})
+
 export default function Home() {
   return (
     <div>
-     <h1>HELLO WORLD!</h1>
+      <h1>HELLO WORLD!</h1>
+      <Button>ENVIAR</Button>
     </div>
   )
 }

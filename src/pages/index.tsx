c4403e6442749595ad7161a72dd5291b5de79e38
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-20 10:04:39 -0300

    lesson: create styles of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import { styled } from '../styles'
-
-const Button = styled('button', {
-  backgroundColor: '$green300',
-})
+import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/Home'
+import shirt1 from '../assets/Shirt-1.png'
+import shirt2 from '../assets/Shirt-2.png'
+// import shirt3 from '../assets/Shirt-3.png'
 
 export default function Home() {
   return (
-    <div>
-      <h1>HELLO WORLD!</h1>
-      <Button>ENVIAR</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }

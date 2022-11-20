4e5acd4dd72b044b5eda012676abf621c3479497
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-20 09:04:22 -0300

    lesson: create header of app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import { globalStyles } from '../styles/global'
+import logoImage from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/App'
 
 globalStyles()
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }

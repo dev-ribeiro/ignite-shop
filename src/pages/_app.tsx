c0b52d0427aa6ae0b5c3f0b256cb44441ae6d751
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-20 09:14:26 -0300

    lesson: next image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { globalStyles } from '../styles/global'
 import logoImage from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/App'
+import Image from 'next/image'
 
 globalStyles()
 
@@ -8,7 +9,7 @@ export default function App({ Component, pageProps }) {
   return (
     <Container>
       <Header>
-        <img src={logoImage.src} alt="" />
+        <Image src={logoImage} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>

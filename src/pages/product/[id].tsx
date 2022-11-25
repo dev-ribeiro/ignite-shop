d61628b9bad71c5280123a1927161ca5f566ad2d
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-11-25 20:16:04 -0300

    lesson: use ssg

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,8 @@
-import { useRouter } from "next/router"
+import { useRouter } from 'next/router'
 
-export default function Product(){
-    const {query} = useRouter()
-    console.log(query)
+export default function Product() {
+  const { query } = useRouter()
+  console.log(query)
 
-    return (
-        <h1>Product</h1>
-    )
+  return <h1>Product</h1>
 }

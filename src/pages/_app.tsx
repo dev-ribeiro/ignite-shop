dd4860b5e2b296538913e4251d6686284cd5e3d1
David Ribeiro <david.ribeiro.professional@gmail.com>
2022-12-03 10:27:24 -0300

    styles: add hand bag icon in Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { globalStyles } from '../styles/global'
 import logoImage from '../assets/logo.svg'
-import { Container, Header } from '../styles/pages/App'
+import { Container, Header, IconWrapper } from '../styles/pages/App'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Handbag } from 'phosphor-react'
 
 globalStyles()
 
@@ -9,7 +11,12 @@ export default function App({ Component, pageProps }) {
   return (
     <Container>
       <Header>
-        <Image src={logoImage} alt="" />
+        <Link href="/" prefetch={false}>
+          <Image src={logoImage} alt="" />
+        </Link>
+        <IconWrapper>
+          <Handbag size={24} />
+        </IconWrapper>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/App.ts
+++ src/styles/pages/App.ts
@@ -13,4 +13,13 @@ export const Header = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+})
+
+export const IconWrapper = styled('div', {
+  background: '#202424',
+  padding: '0.75rem',
+  borderRadius: 6,
 })
